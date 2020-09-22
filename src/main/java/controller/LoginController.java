bf5416ec13145b52433ea92edd9ca5321988ea6c
Nikhil <vallalanikhil@gmail.com>
2020-09-22 12:21:14 +0530

    done

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -15,34 +15,35 @@ import service.UserService;
 
 @Controller
 public class LoginController {
-     
-	
+
 	User user;
 	@Autowired
 	UserService userService;
 	
-	@RequestMapping(value="/home",method=RequestMethod.GET)
+	@RequestMapping(value="/home", method=RequestMethod.GET)
 	public ModelAndView showHome() {
-		ModelAndView mav= new ModelAndView("home");
+		ModelAndView mav = new ModelAndView("home");
 		return mav;
+		
 	}
-	@RequestMapping(value="/login",method=RequestMethod.GET)
+	@RequestMapping(value="/login", method=RequestMethod.GET)
 	public ModelAndView showLogin() {
-		ModelAndView mav= new ModelAndView("home");
+		ModelAndView mav = new ModelAndView("login");
 		return mav;
+		
 	}
 	
-	@RequestMapping(value="/loginProcess",method=RequestMethod.POST)
-	public ModelAndView loginProcess(HttpServletRequest  request,HttpServletResponse  response,Login login) {
-		ModelAndView mav= null;
-		
-		user=userService.validateUser(login);
+	@RequestMapping(value="/loginProcess", method=RequestMethod.POST)
+	public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response, Login login) {
+		ModelAndView mav = null;
+		user = userService.validateUser(login);
 		if(user!=null) {
-			mav= new ModelAndView("welcome","user",user.getFirstname());
-		}else
-		{
-			mav= new ModelAndView("login","message","Username or Password is wrong");
+			mav = new ModelAndView("welcome", "user", user.getFirstname());
+		}
+		else {
+			mav = new ModelAndView("login","message", "Wrong credentials"); 
 		}
 		return mav;
+		
 	}
 }
